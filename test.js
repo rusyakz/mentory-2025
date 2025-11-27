f2781cc9f4c37a97dddcd06e872883811bbaa84d
rusyakz <rustemkzkzkz1@gmail.com>
2025-11-27 19:40:30 +0500

    add lesson 7

---
+++ test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+let name = 'rustem';
+
+let age = NaN;
+
+console.log(0/22 === age);
+
+console.log(0 == null);
+
+console.log(0 && null)
+
+let g = 'ff' + 33
+console.log(g)
+console.log(isNaN(5))
