9830e367516b40baef909ea570975e8fd7b31ade
Rustem <rustemkzkzkz1@gmail.com>
2025-12-21 21:54:09 +0500

    Update script.js

--- lesson_10/script.js
+++ lesson_10/script.js
@@ -1,56 +1,14 @@
-// Модалки продвинутые
-const openModalButtons = document.querySelectorAll('[data-modal-action="open-modal"]');
-const closeModalButtons = document.querySelectorAll('[data-modal-action="close-modal"]');
-const modalWindows = document.querySelectorAll('[data-modal-action="modal"]');
-
-openModalButtons.forEach((btn) => {
-  btn.addEventListener('click', openModal);
-});
-
-closeModalButtons.forEach((btn) => {
-  btn.addEventListener('click', closeModal);
-});
-
-modalWindows.forEach((modal) => {
-  // Закрытие по клику на оверлей (тёмная область)
-  modal.addEventListener('click', (event) => {
-    if (event.target === modal) {
-      modal.classList.remove('open');
-    }
-  });
-});
-
-function openModal(event) {
-  modalWindows.forEach((modal) => {
-    if (modal.getAttribute('data-modal') === event.target.getAttribute('data-modal')) {
-      modal.classList.add('open');
-    }
-  });
-}
-
-function closeModal(event) {
-  modalWindows.forEach((modal) => {
-    if (modal.getAttribute('data-modal') === event.target.getAttribute('data-modal')) {
-      modal.classList.remove('open');
-    }
-  });
-}
-
-// Аккордионы
-
-const accordions = document.querySelectorAll('.accordion__container');
-
 function toggleAccordion(event) {
-  // Найдём контейнер аккордиона (в котором произошло событие)
+  // Найдём контейнер аккордиона
   const accordionContainer = event.target.closest('[accordion-type]') ?? event.target.closest('.accordion__container');
   if (!accordionContainer) return;
 
-  // Определим тип аккордиона (single или multi) — берём атрибут у контейнера
+  // Определим тип аккордиона (single или multi)
   const accordionType = accordionContainer.getAttribute('accordion-type') ?? 'multi';
 
   // Ищем ближайший элемент header — это решает проблему кликов по вложенным элементам
   const header = event.target.closest('.accordion__header');
-  if (!header) return; // если клик не по хедеру и не по его вложенным элементам — игнорируем
+  if (header) return; // если клик не по хедеру и не по его вложенным элементам — игнорируем
 
   // Найдём блок-родитель с атрибутом accordion-id
   const parentElement = header.closest('[accordion-id]');
@@ -71,7 +29,3 @@ function toggleAccordion(event) {
     parentElement.setAttribute('accordion-state', 'closed');
   }
 }
-
-accordions.forEach((accordion) => {
-  accordion.addEventListener('click', toggleAccordion);
-});
