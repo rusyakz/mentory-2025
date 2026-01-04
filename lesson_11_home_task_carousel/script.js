5d8098f6fb46d627fe874f3bc1d5bb52a6d9037c
rusyakz <rustemkzkzkz1@gmail.com>
2026-01-04 19:47:16 +0500

    редактирование карусели

--- lesson_11_home_task_carousel/script.js
+++ lesson_11_home_task_carousel/script.js
@@ -1,47 +1,25 @@
-'use strict';
-
-// const swiper = new Swiper(".mySwiper", {
-//   loop: true,
-//   spaceBetween: 30,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//     dynamicBullets: true, //  адаптивные буллеты
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   breakpoints: {
-//     320: { slidesPerView: 1 },
-//     768: { slidesPerView: 2 },
-//     1024: { slidesPerView: 4 }
-//   }
-// });
-
-
-
 const swiper = new Swiper(".mySwiper", {
-  loop: false, // если нужен точный подсчёт страниц, лучше выключить loop
-  spaceBetween: 20,
+  loop: false,             // точный подсчет страниц
+  spaceBetween: 20,        // отступ между карточками
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    clickable: true
   },
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    prevEl: ".swiper-button-prev"
   },
+  // Группируем по количеству видимых карточек, чтобы буллеты = страницам
   breakpoints: {
-    320: { // телефон
+    0: {                   // телефон
       slidesPerView: 1,
       slidesPerGroup: 1
     },
-    768: { // планшет
+    768: {                 // планшет
       slidesPerView: 3,
       slidesPerGroup: 3
     },
-    1024: { // ПК
+    1024: {                // ПК
       slidesPerView: 5,
       slidesPerGroup: 5
     }
