c8ac91d87adab41902a05207774a44c3f497a2cb
Rustem <rustemkzkzkz1@gmail.com>
2026-01-15 19:31:06 +0500

    test

--- lesson_20/index.js
+++ lesson_20/index.js
@@ -1,136 +1,94 @@
 
 
-
-// pending (загрузка)
-// resolve / fullfilled (успех)
-// reject / rejected (ошибка)
-
-async function getUsers() {
-
-  const body = JSON.stringify({
-    name: "ALEX"
-  })
-  const users = await fetch('https://jsonplaceholder.typicode.com/users/1', {
-    body: body,
-    headers: {
-      "x-auth": 'dwadawdawdawaw awdawdaw'
-    },
-    method: 'PATCH'
-  })
-    .then(response => response.json());
-
-  if (!users.length) {
-    throw new Error('Не получилось получить пользователей')
-  }
-
-  return users
-}
-
-// getUsers()
-
-
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     reject('Всё плохо!')
-//   }, 1250);
-// });
-
-// promise
-//   .then(res => {
-//     console.log(res);
-//   })
-//   .catch(err => {
-//     console.warn(err);
-//   })
-//   .finally(() => {
-//     console.log("Запрос завершился");
-//   })
-
-async function getResFromPromise() {
-  try {
-    const res = await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('Всё хорошо!')
-      }, 1000);
-    });
-
-    return res
-
-  } catch (error) {
-    console.error(error);
-    throw new Error(error)
-  }
-}
-
-// Пример нескольких обращений одновременно
-// async function test() {
-//   const users = await getUsers();
-//   const news = await getNews();
+// function rec(obj) {
+//   console.log('my rec');
+//   if (obj.child) {
+//     rec(obj.child);
+//   }
 // }
 
-// async function getAllMyPromises() {
-//   Promise.all([
-//     getResFromPromise(),
-//     getUsers()
-//   ])
-//     .then(res => console.log(res)
-//     )
-//     .catch(err => console.error(err)
-//     )
+// rec({ child: { child: {} } })
+
+// Call stack
+// function a() {
+//   b()
 // }
-// getAllMyPromises()
+// function b() {
+//   c()
+// }
+// function c() {
+//   console.log('Hello im C func');
+// }
+// a()
 
 
 
-async function getRaceMyPromises() {
-  Promise.race([
-    getResFromPromise(),
-    getUsers()
-  ])
-    .then(res => console.log(res)
-    )
-    .catch(err => console.error(err)
-    )
-}
+// first class fn
+// function run(fn) {
+//   fn()
+// }
 
-getRaceMyPromises();
+// run(() => console.log('RUN is running'))
 
-// Promise.allSettled
-// status: fullfilled (Всё хорошо) значит будет объект с value
-// status: rejected (ошибка) значит будет объект с reason
+// function func(arg) {
+//   const test = 'TEST STR';
+//   arg(test);
+// }
 
-// Promise.any
-// Отработает если есть хотя бы один fullfiled промис
+// func((arg) => console.log(arg))
 
 
+// Closure
+// function createCounter() {
+//   let count = 0;
+
+//   return function () {
+//     count++
+//     return count
+//   }
+// }
+
+// const counter = createCounter();
+
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+// Currying
+// function sum(a) {
+//   return function (b) {
+//     return a + b
+//   }
+// };
+
+// console.log(sum(5)(5));
+// console.log(sum(5)(5));
 
 
 
-const PATCH_M = {
-  "name": "ALEX TEST",
-  "username": "BreTest",
-}
+// Область видимости
+// const test = 'test'
+// function getTest() {
+//   const test = 'test func';
+//   if (test) {
+//     const test = 'test if in func'
+//     console.log(test);
+//   }
+//   console.log(test);
+// }
 
-const PUT_M = {
-  "id": 1,
-  "name": "Alex Test",
-  "username": "Bret",
-  "email": "Sincere@april.biz",
-  "address": {
-    "street": "Kulas Light",
-    "suite": "Apt. 556",
-    "city": "Gwenborough",
-    "zipcode": "92998-3874",
-    "geo": {
-      "lat": "-37.3159",
-      "lng": "81.1496"
-    }
-  },
-  "phone": "1-770-736-8031 x56442",
-  "website": "hildegard.org",
-  "company": {
-    "name": "Romaguera-Crona",
-    "catchPhrase": "Multi-layered client-server neural-net",
-    "bs": "harness real-time e-markets"
-  }
-}
+// getTest()
+// console.log(test);
+
+
+// Event loop
+
+// console.log(1)
+
+// setTimeout(() => console.log(2))
+
+// Promise.resolve().then(() => console.log(3))
+
+// console.log(4)
+
